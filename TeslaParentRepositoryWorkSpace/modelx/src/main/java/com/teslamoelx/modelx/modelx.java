ef4a27eea08a9743b03056a405714ef02d5e68fc
Slim <Slim@Admin>
2021-10-11 13:08:47 +0530

    new sysout added

--- TeslaParentRepositoryWorkSpace/modelx/src/main/java/com/teslamoelx/modelx/modelx.java
+++ TeslaParentRepositoryWorkSpace/modelx/src/main/java/com/teslamoelx/modelx/modelx.java
@@ -9,5 +9,6 @@ public class modelx
     public static void main( String[] args )
     {
         System.out.println( "Hello World! this is modelx" );
+        System.out.println("hello 1");
     }
 }
