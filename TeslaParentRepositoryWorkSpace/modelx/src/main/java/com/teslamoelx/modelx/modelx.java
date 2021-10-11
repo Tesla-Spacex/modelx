c02e72af4a990e20dfa74dce43499bf53f6f7951
Slim <Slim@Admin>
2021-10-11 12:58:06 +0530

    This is parent push by tesla

---
+++ TeslaParentRepositoryWorkSpace/modelx/src/main/java/com/teslamoelx/modelx/modelx.java
@@ -0,0 +1,13 @@
+package com.teslamoelx.modelx;
+
+/**
+ * Hello world!
+ *
+ */
+public class modelx 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! this is modelx" );
+    }
+}
